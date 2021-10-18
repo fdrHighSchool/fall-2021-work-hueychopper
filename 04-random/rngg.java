18ff5c5022b917acddb4f67cd474f7a3e6b83bc5
hueychopper <codelearn370@gmail.com>
2021-10-18 09:33:37 -0400

    modes update. command interface

--- 04-random/rngg.java
+++ 04-random/rngg.java
@@ -3,29 +3,67 @@ import java.util.ArrayList;
 
 class Rngg {
     public static void main(String[] args) {
-        ArrayList<Integer> numbersGuessed = new ArrayList<Integer>();
+        //TODO: replay, check: difficulty = {"easy", "medium", "hard"}, 
         System.out.println("use ctrl+c to exit");
-
-        int correctNum = 5;
-        boolean guessed = false;
+        try {
+            if(args[0].equals("easy") || args[0].equals("medium") || args[0].equals("hard")) {
+                gameMode(args[0]);
+            } 
+        } catch(Exception except) {
+            System.out.println("none chosen. exiting");
+            System.exit(0);
+        }
+        System.out.println("that is the correct number");
         
+        
+    }
+    public static void gameMode(String mode) {
+        System.out.println(mode);
+        if(mode.equals("easy")) {
+            Easy(mode);
+        } else if(mode.equals("medium")) {
+            Med(mode);
+        } else {
+            Hard(mode);
+        }
+    }
+    public static void mainLoop(int correctNum, String mode) {
+        ArrayList<Integer> numbersGuessed = new ArrayList<Integer>();
+        boolean guessed = false;
+
+        System.out.println(correctNum);
+        System.out.println(mode);
 
         while(guessed == false) {
             System.out.print("enter number: ");
             Scanner sc = new Scanner(System.in);
             int userInput = sc.nextInt();
             numbersGuessed.add(sc.nextInt());
-            System.out.println(numbersGuessed);
+            // System.out.println(numbersGuessed);
+            if(userInput > correctNum && userInput <= (correctNum+2)) {
+                System.out.println("close. A little less");
+            } 
+            if(userInput > 1) {
+                if(userInput < correctNum && userInput >= (correctNum-2)) {
+                    System.out.println("close. a little more");
+                }
+            }
 
             if(userInput == correctNum) {
                 guessed = true;
-                System.out.println("thats the number");
                 break;
-            } else if(userInput < correctNum && userInput > 3) {
-                System.out.println("Close. A little more");
-            } else if(userInput > correctNum && userInput < 7) {
-                System.out.println("Close. A little less");
-            } 
+            }
         }
+    }
+    public static void Easy(String modeInherit) {
+        int correctNum = (int)(Math.random()*9+1);
+        // System.out.println(correctNum);
+        mainLoop(correctNum, modeInherit);
+    }
+    public static void Med(String modeInherit) {
+        System.out.println("med");
+    }
+    public static void Hard(String modeInherit) {
+        System.out.println("hard");
     }
 }
