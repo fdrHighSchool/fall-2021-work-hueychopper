11c2f07cdbe46bdfaa4a0a5ec3ad10efdeaa3e36
hueychopper <codelearn370@gmail.com>
2021-11-29 21:41:05 -0500

    conversion+fraction settings

--- 06-fractions/Frac2.java
+++ 06-fractions/Frac2.java
@@ -36,23 +36,90 @@ public class Fraction {
                 System.out.println("");
             }
         }
-        if(fractions.contains("+")) {
-            fractions.remove(fractions.indexOf("+"));
-            System.out.println(fractions);
+        cutFraction(fractions);
+    }
+    public static void cutFraction(ArrayList<String> freshFractions) {
+        //corresponding numerators and denominators
+        //undecided whole numbers\
+        ArrayList<Integer> wholes = new ArrayList<Integer>();
+        ArrayList<Integer> numers = new ArrayList<Integer>();
+        ArrayList<Integer> denoms = new ArrayList<Integer>();
+        ArrayList<String> ops = new ArrayList<String>();
+        for(int i = 0; i < freshFractions.size(); i++) {
+            if(freshFractions.get(i).contains("+") || freshFractions.get(i).contains("-")) {
+                ops.add(freshFractions.get(i));
+                freshFractions.remove(i);
+                // System.out.println(freshFractions);
+            }
+        }
+        for(int s = 0; s < freshFractions.size(); s++) {
+            int f_s = freshFractions.get(s).indexOf("/");
+            int u = freshFractions.get(s).indexOf("_");
+            int num, den, whole;
+            if(u != -1) {
+                whole = getWhole(freshFractions.get(s), u);
+                num = getNumer(freshFractions.get(s), f_s, u);
+                den = getDenom(freshFractions.get(s), f_s);
+            } else {
+                if(f_s != -1) {
+                    whole = 0;
+                    num = getNumer(freshFractions.get(s), f_s, u);
+                    den = getDenom(freshFractions.get(s), f_s);
+                } else {
+                    whole = 0;
+                    num = Integer.parseInt(freshFractions.get(s).substring(0));
+                    den = 1;
+                }
+            }
+            wholes.add(whole);
+            numers.add(num);
+            denoms.add(den);
+            evalFractions(freshFractions, ops, numers, denoms, wholes);
         }
     }
     public static int getDenom(String pt, int cf) {
         int denom = Integer.parseInt(pt.substring(cf + 1));
+        System.out.println("denomerator: "+Integer.toString(denom));
         return denom;
     }
     public static int getNumer(String pt, int cf, int ix) {
         int numerator = Integer.parseInt(pt.substring(ix + 1, cf));
+        System.out.println("numerator: "+Integer.toString(numerator));
         return numerator;
     }
     public static int getWhole(String pt, int index) {
         int wholeNum = Integer.parseInt(pt.substring(0, index));
+        System.out.println("whole: "+Integer.toString(wholeNum));
         return wholeNum;
     }
+
+    public static void evalFractions(
+        ArrayList<String> fracton,
+        ArrayList<String> ops, 
+        ArrayList<Integer> nums, 
+        ArrayList<Integer> dens, ArrayList<Integer> whole) {
+            for(int i = 0; i < whole.size(); i++) {
+                String impFrac = convertToIrregular(whole.get(i), nums.get(i), dens.get(i));
+                if(whole.get(i) != 0) {
+                    whole.set(i, 0);
+                    nums.set(i, getNumer(impFrac, impFrac.indexOf("/"), impFrac.indexOf("_")));
+                }
+            }
+    }
+    public static void checkOps(
+        ArrayList<String> ops,
+        ArrayList<Integer> nums,
+        ArrayList<Integer> dens
+    ) {
+        System.out.println("check ops pass");
+    }
+    public static String convertToIrregular(int whole, int num, int den) {
+        int nN = (den * whole) + num;
+        System.out.println("conversion[m:i]: "+Integer.toString(nN)+"/"+Integer.toString(den));
+        return Integer.toString(nN)+"/"+Integer.toString(den);
+    }
+
+
     public static int greatestCommonDivisor(int a, int b) { //numerator, denominator
         //euclidean algorithm
         int gcd;
@@ -62,14 +129,14 @@ public class Fraction {
         } else if(b == 0) {
             gcd = a; //if b is 0 then a is the GCF
             return gcd;
-        } else if(a != 0 && b != 0) { //if none are 0 we must solve for the GCF
-            while(a != 0 || b != 0) { //loop until a or b is 0 in which case, the other that isn't 0 is the GCF
-                int q = a / b; //get quotient of these two params
-                int r = a % b; //get remainder with modulo
-                if(b * q + r == a) { //accroding to the formula a has to be equal to b*q+r in order to find GCF
-                    a = b; //reduce a to b
-                    b = r; //set b to remainder so next loop a will get former r and b will be new r
-                    if(a == 0 && b != 0 || b == 0 && a != 0) { //check if loop has reduced a or b to 0
+        } else if(a != 0 && b != 0) {
+            while(a != 0 || b != 0) { 
+                int q = a / b;
+                int r = a % b; 
+                if(b * q + r == a) { 
+                    a = b;
+                    b = r; 
+                    if(a == 0 && b != 0 || b == 0 && a != 0) {
                         if(a != 0) { //b is 0
                             gcd = a;
                             return a;
@@ -77,7 +144,7 @@ public class Fraction {
                             gcd = b;
                             return b;
                         } else {
-                            break; //exit loop return nothing
+                            break;
                         }
                     }
                 }
@@ -94,3 +161,4 @@ public class Fraction {
         return lcm;
     }
 }
+//TODO: verbose output
