47c679146971f2d2c1860c007dcb957d0150e475
hueychopper <codelearn370@gmail.com>
2022-01-03 18:09:28 -0500

    classwork exams

---
+++ 08-exams/Exams.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Exams {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+
+    // sample student with 5 slots for grades
+    int[] sampleStudent = new int[5]; //0,1,2,3,4,5
+
+    // fill up the "gradebook"
+    for(int i = 0; i < sampleStudent.length; i++) {
+      sampleStudent[i] = (int)(Math.random() * 46) + 55;
+    } // end for loop
+
+    // display the first 5 grades
+    System.out.println(Arrays.toString(sampleStudent)+"<--sample student array");
+    appendTo(sampleStudent);
+  } // end main method
+  public static void appendTo(int[] arr) {
+    int[] inh = new int[6];
+    for(int i = 0; i < arr.length; i++) {
+      inh[i] = arr[i];
+    }
+    inh[inh.length-1] = (int)(Math.random() * 46) + 55;
+    System.out.println(Arrays.toString(inh));
+    lingrade(inh);
+  }
+  public static void lingrade(int[] oArr) {
+    int min = oArr[0];
+    for(int i = 0; i < oArr.length; i++) {
+      if(min > oArr[i]) min = oArr[i];
+    }
+    int sGrade = oArr[oArr.length - 1];
+    if(sGrade != min || sGrade > min) {
+      for(int i = 0; i < oArr.length; i++) {
+        if(min == oArr[i]) {
+          oArr[i] = sGrade; //replace
+        }
+      }
+    }
+    System.out.println(Arrays.toString(oArr));
+  }
+
+/*
+A) Write a method that adds a 6th exam
+to the sample student’s set of grades.
+*/
+
+/*
+B) Write a method that takes the original set
+of 5 grades and replaces the lowest grade if
+the 6th grade is greater than it.
+
+If it is not, then the original 5 grades remain.
+*/
+
+
+} // end class
