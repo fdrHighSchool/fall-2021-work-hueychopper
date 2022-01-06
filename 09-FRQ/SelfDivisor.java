6098ab80a5250526adb4c41554af5179d14d3449
hueychopper <codelearn370@gmail.com>
2022-01-05 20:37:06 -0500

    FRQ answer attempt

--- 09-FRQ/SelfDivisor.java
+++ 09-FRQ/SelfDivisor.java
@@ -2,14 +2,13 @@ import java.util.Arrays;
 
 public class SelfDivisor {
     public static void main(String[] args) {
-        // System.out.println(isSelfDivisor(26));
         firstNumSelfDivisors(10, 3);
     }
     public static boolean isSelfDivisor(int number) {
         int pch = number;
         while(number > 0) {
             int digit = number % 10;
-            if(pch % digit != 0 || digit == 0) {
+            if(pch % digit != 0) {
                 return false;
             }
             number = number / 10;
