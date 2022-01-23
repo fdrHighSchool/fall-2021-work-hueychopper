08cb1c299820ec83fcaedcae712897ecee15e962
hueychopper <codelearn370@gmail.com>
2022-01-22 19:41:30 -0500

    connect4

--- 11-c4/Connect.java
+++ 11-c4/Connect.java
@@ -41,37 +41,57 @@ public class Connect {
         String uchar = coords.substring(0,com);
         int colNum = Integer.parseInt(coords.substring(com+1));
         
-        String winner = "";
+        String winner1 = "";
+        String winner2 = "";
         for(int row = bd.length-1; row > -1; row--) {
             if(bd[row][colNum] == "[ ]") {
                 bd[row][colNum] = "["+uchar+"]";
                 formatBoard(bd);
-                winner = checkWinner(bd);
+                winner1 = checkWinner(bd, colNum, "[x]");
+                winner2 = checkWinner(bd, colNum, "[o]");
                 break;
             }
-            // System.out.print();
-            System.out.println("\033[0;32m"+winner);
+            System.out.println(winner1);
+            System.out.println(winner2);
         }
-        return winner;
-        
+        if(!winner1.equals("0")) {
+            return winner1;
+        } else if(!winner2.equals("0")) {
+            return winner2;
+        }
+        return "blank";
     }
 
-    public static String checkWinner(String[][] bd) {
+    public static String checkWinner(String[][] bd, int cCol, String uchar) {
         for(int row = 0; row < bd.length; row++) {
-            int rowCountX = 0; 
-            int rowCountO = 0;
             for(int col = 0; col < bd[row].length; col++) {
-                
-                if(bd[row][col].equals("[x]")) {
-                    rowCountX++;
-                } else {
-                    rowCountX = 0;
-                }
-                
-
-
-                if(rowCountX == 4) {
-                    return "player1";
+                String sEl = bd[row][col];
+                if(sEl.equals(uchar)) {
+                    //horizontal
+                    if(col <= bd[row].length-4 && sEl.equals(bd[row][col+1]) && sEl.equals(bd[row][col+2]) && sEl.equals(bd[row][col+3])) {
+                        System.out.println(uchar+" win horizontal");
+                    }
+                    //vertical
+                    if(row <= bd.length-4 && sEl.equals(bd[row+1][col]) && sEl.equals(bd[row+2][col]) && sEl.equals(bd[row+3][col])) {
+                        System.out.println(uchar+" win vertical");
+                    }
+                    //right to left
+                    for(int i = 3; i < bd.length; i++) {//set row bounds
+                        for(int j = 0; j < bd[row].length-3; j++){ //set col bounds
+                            if (bd[i][j].equals(uchar) && bd[i-1][j+1].equals(uchar) && bd[i-2][j+2].equals(uchar)
+                            && bd[i-3][j+3].equals(uchar) ) {
+                                System.out.println(uchar+" diagonal win");
+                            }
+                        }
+                    }
+                    //left to right
+                    for(int i = 3; i < bd.length; i++) {
+                        for(int j = 3; j < bd[row].length-3; j++) {
+                            if(bd[i][j].equals(uchar) && bd[i-1][j-1].equals(uchar) && bd[i-2][j-2].equals(uchar) && bd[i-3][j-3].equals(uchar) ) {
+                                System.out.println(uchar+" diagonal win");
+                            }
+                        }
+                    }
                 }
             }
         }
